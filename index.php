f8d739c57e52eedddd0ffeef6fffbb2f6ef5929f
cTurtle98 <cturtle98@98seaturtles.net>
2017-02-20 01:39:07 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>98SeaTurtles Network</title>
+        <meta name="description" content="The Projects of Ciaran Farley">
+        <link rel="stylesheet" href="/css/main.css">
+		<?php include $_SERVER['DOCUMENT_ROOT'] . "/include/head.php"; ?>
+    </head>
+    <body>
+        <?php
+			include $_SERVER['DOCUMENT_ROOT'] . "/include/header.html";
+            include $_SERVER['DOCUMENT_ROOT'] . "/include/menu.html";
+            include $_SERVER['DOCUMENT_ROOT'] . "/include/ad/right-collum.php";
+        ?>
+        <div id="content">
+			<div id="content-title">
+				Home
+			</div>
+			<div id="content-body">
+            	Welcome to the 98SeaTurtles Website
+            	<br>
+            	This website is a collection of Ciarán Farley's projects and hobbies
+                <br><br>
+                Check out my <a href="http://ciaranfarley.info" target="_blank">Resumé Website</a>
+				<br><br>
+				This website is and always will be a work in progress. Come back again and see what has changed.
+                <br><br>
+                <div id="content-subtitle">Featured Pages</div>
+                <ul>
+                    <li><a href="wishlist/2016christmas/">2016 Christmas Wishlist</a></li>
+                    <li><a href="social/ciaran/">Ciaran's Social Accounts</a></li>
+                </ul>
+                <br>
+                <div id="content-subtitle">Credits</div>
+                Special Thanks to <b>Satyam Love</b> of <a href="http://oneofthesevenbillion.com" target="_blank">OneOfTheSevenbillion</a> for helping me with adding php functionality to the site and for helping me with the program to make the meme and photography pages work. <br>
+                <br>
+                Special thanks to <b>Nick Stefanisko</b> of <a href="http://nick.stefanisko.net" target="_blank">nick.stefanisko.net</a> for helping me with attempting to make the menu fancy (I failed not him) and giving me advice on file size for the photography page. And for hepling me with adding the last edited date to the footer.
+				<br><br><br><br>
+                <div id="content-subtitle">Powered By</div>
+                <a href="http://apache.org"><img src="include/Apache_PoweredBy.svg" height="100px"></a>
+                <a href="http://debian.org"><img src="/include/debianlogo.svg" height="100px"></a> <br>
+                <br>
+			</div>
+            <div class="fb-comments" data-href="http://98seaturtles.net" data-numposts="5"></div>
+        </div>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/include/footer.html";?>
+    </body>
+</html>
